a7c3ef3768f7b97453f4fcc5957b442ad8a4a128
YS739 <suk921@gmail.com>
2023-01-31 12:10:53 +0900

    Test Dynamic Routes

---
+++ pages/post/[id].js
@@ -0,0 +1,9 @@
+import { useRouter } from "next/router";
+
+const Post = () => {
+  const router = useRouter();
+  const { id } = router.query;
+  return <div>Post: {id}</div>;
+};
+
+export default Post;
