acacb54e33e92ef8b9f531639220f8607c94d648
YS739 <suk921@gmail.com>
2023-02-01 12:31:01 +0900

    Test revalidate:

--- pages/ssg/[id].js
+++ pages/ssg/[id].js
@@ -31,20 +31,19 @@ export const getStaticPaths = async () => {
         params: { id: "3" },
       },
     ],
-    fallback: true,
+    fallback: false,
   };
 };
 
 export async function getStaticProps(context) {
   const { id } = context.params;
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/posts/${id}`
-  );
+  const response = await fetch(`http://localhost:3001/posts/${id}`);
   const post = await response.json();
 
   return {
     props: {
       post,
     },
+    revalidate: 3,
   };
 }
--- pages/ssr/[id].js
+++ pages/ssr/[id].js
@@ -19,9 +19,7 @@ export async function getServerSideProps(context) {
   const { params } = context;
   const { id } = params;
 
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/posts/${id}`
-  );
+  const response = await fetch(`http://localhost:3001/posts/${id}`);
   const post = await response.json();
 
   return {
