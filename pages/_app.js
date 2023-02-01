a65066f7c4f86a08f9a140469ee4753c719c5f7d
YS739 <suk921@gmail.com>
2023-02-01 14:02:28 +0900

    Feat meta tag with og site

--- pages/_app.js
+++ pages/_app.js
@@ -8,8 +8,32 @@ const App = ({ Component, pageProps }) => {
   return (
     <div>
       <Head>
-        <title>Test입니다.</title>
-        <meta name="description" content="설명입니다." />
+        <title>Next.js</title>
+        <meta name="description" content="Next.js 기본 강의 - 연습 페이지" />
+        <meta property="og:url" content="https://www.naver.com/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Next.js" />
+        <meta
+          property="og:description"
+          content="Next.js 기본 강의 - 연습 페이지"
+        />
+        <meta
+          property="og:image"
+          content="https://user-images.githubusercontent.com/112805225/215955489-b329cbf5-9b72-4752-bdb2-90ea70c53d40.png"
+        />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="naver.com" />
+        <meta property="twitter:url" content="https://www.naver.com/" />
+        <meta name="twitter:title" content="Next.js" />
+        <meta
+          name="twitter:description"
+          content="Next.js 기본 강의 - 연습 페이지"
+        />
+        <meta
+          name="twitter:image"
+          content="https://user-images.githubusercontent.com/112805225/215955489-b329cbf5-9b72-4752-bdb2-90ea70c53d40.png"
+        />
       </Head>
       <div>
         <h2>Navigation</h2>
