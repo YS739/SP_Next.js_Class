fca54739030950bf2a0d62e30e907e630cbd0fe7
YS739 <suk921@gmail.com>
2023-02-01 10:09:04 +0900

    Test fallback is false

---
+++ pages/ssg/[id].js
@@ -0,0 +1,50 @@
+import { useRouter } from "next/router";
+
+const Post = ({ post }) => {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      Post: {router.asPath}
+      <h2>{post.title}</h2>
+      <p>{post.body}</p>
+    </div>
+  );
+};
+
+export default Post;
+
+export const getStaticPaths = async () => {
+  return {
+    paths: [
+      {
+        params: { id: "1" },
+      },
+      {
+        params: { id: "2" },
+      },
+      {
+        params: { id: "3" },
+      },
+    ],
+    fallback: false,
+  };
+};
+
+export async function getStaticProps(context) {
+  const { id } = context.params;
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${id}`
+  );
+  const post = await response.json();
+
+  return {
+    props: {
+      post,
+    },
+  };
+}
